e358d770b6b2e8153554e7e89f13baeda8a97769
Yuliia Kuzmenko <4projectsanduni@gmail.com>
2026-04-27 10:29:08 +0200

    Generacja miast (randomowo)

--- GUI.py
+++ GUI.py
@@ -1,11 +1,30 @@
 import tkinter as tk
 from tkinter import ttk, Button
-
+from core.graphs import Graphs
 
 class Window(tk.Tk):
+    def add_cities(self):
+        try:
+            input_value = self.random_gen_entry.get()
+            if not input_value:
+                print("Pole jest puste.")
+                return
+
+            ile = int(input_value)
+            if ile > 2000:
+                ile = 2000
+                self.random_gen_entry.delete(0, tk.END)
+                self.random_gen_entry.insert(0, "2000")
+
+            self.graph_manager.cities_random(ile)
+
+        except ValueError:
+            from tkinter import messagebox
+            messagebox.showwarning("Błąd. Wpisz liczbę całkowitą!")
+
     def __init__(self):
         super().__init__()
-        self.title("TSP Metaheuristic Solver")
+        self.title("Środowisko testowe TSP")
         self.geometry("1500x900")
         #----------------------------------Frame division---------------------------------------------
         self.left_panel = tk.Frame(self, width=300, bg="#f0f0f0", padx=10, pady=10)
@@ -22,36 +41,36 @@ class Window(tk.Tk):
 
         # ----------------------------------Frame fulfilling---------------------------------------------
         #Left panel
-        tk.Label(self.left_panel, text="Canvas Settings", font=("Arial", 14, "bold"), bg="#f0f0f0").pack(pady=15)
+        tk.Label(self.left_panel, text="Ustawienia Wykresów", font=("Arial", 14, "bold"), bg="#f0f0f0").pack(pady=15)
         #-------------------------------------------------------------------------------------------------------------------
-        gen_frame = tk.LabelFrame(self.left_panel, text=" City generation ", font=("Arial", 10, "bold"), bg="#f0f0f0",
+        gen_frame = tk.LabelFrame(self.left_panel, text=" Generacja miast ", font=("Arial", 10, "bold"), bg="#f0f0f0",
                                   padx=10, pady=10)
         gen_frame.pack(fill=tk.X, padx=10, pady=5)
 
-        tk.Label(gen_frame, text="Random generation (max 2000):", bg="#f0f0f0").pack(anchor="w")
+        tk.Label(gen_frame, text="Randomna generacja (max 2000):", bg="#f0f0f0").pack(anchor="w")
         self.random_gen_entry = tk.Entry(gen_frame, width=15)
         self.random_gen_entry.pack(pady=5, fill=tk.X)
-        tk.Button(gen_frame, text="Add random cities").pack(fill=tk.X)
+        tk.Button(gen_frame, text="Dodać miasta", command=self.add_cities).pack(fill=tk.X)
 
-        tk.Label(gen_frame, text="File operations:", bg="#f0f0f0").pack(anchor="w", pady=(10, 0))
-        self.file_gen_button = tk.Button(gen_frame, text="Upload file (.csv/.txt)")
+        tk.Label(gen_frame, text="Operacje z plikami:", bg="#f0f0f0").pack(anchor="w", pady=(10, 0))
+        self.file_gen_button = tk.Button(gen_frame, text="Nagrać plik (.csv/.txt)")
         self.file_gen_button.pack(fill=tk.X, pady=2)
-        self.manual_button = tk.Button(gen_frame, text="Read manual", font=("Arial", 8, "italic"))
+        self.manual_button = tk.Button(gen_frame, text="Przeczytać manual", font=("Arial", 8, "italic"))
         self.manual_button.pack(anchor="e")
         # -------------------------------------------------------------------------------------------------------------------
-        algo_frame = tk.LabelFrame(self.left_panel, text=" Algorithm Settings ", font=("Arial", 10, "bold"),
+        algo_frame = tk.LabelFrame(self.left_panel, text=" Ustawienia Algoritmów ", font=("Arial", 10, "bold"),
                                    bg="#f0f0f0", padx=10, pady=10)
         algo_frame.pack(fill=tk.X, padx=10, pady=5)
 
-        tk.Label(algo_frame, text="Choose algorithm:", bg="#f0f0f0").pack(anchor="w")
-        self.algorithm_dropdown = ttk.Combobox(algo_frame, values=['Genetic', 'Ant Colony', 'Firefly'],
+        tk.Label(algo_frame, text="Wybrany Algorytm:", bg="#f0f0f0").pack(anchor="w")
+        self.algorithm_dropdown = ttk.Combobox(algo_frame, values=['Genetyczny', 'Mrówkowy', 'Świetlika'],
                                                state="readonly")
         self.algorithm_dropdown.pack(pady=5, fill=tk.X)
         self.algorithm_dropdown.current(0)
 
         self.params_container = tk.Frame(algo_frame, bg="#f0f0f0")
         self.params_container.pack(fill=tk.X, pady=5)
-        tk.Label(self.params_container, text="(Params will appear here)", fg="gray", bg="#f0f0f0").pack()
+        tk.Label(self.params_container, text="(Tu się pojawią parametry)", fg="gray", bg="#f0f0f0").pack()
         # -------------------------------------------------------------------------------------------------------------------
         #tk.Label(self.left_panel, text="Parameters: ", font=("Arial", 11, "bold"), bg="#f0f0f0").pack(pady=5)
         #parametres for genetec
@@ -73,34 +92,34 @@ class Window(tk.Tk):
         self.reset_btn.pack(fill=tk.X, pady=2)
 
         # Botoom panel
-        tk.Label(self.bottom_panel, text="Statistics & Logs", font=("Arial", 12, "bold"), bg="#D8D7DE").grid(row=0, column=0, padx=40, sticky="s")
+        tk.Label(self.bottom_panel, text="Statystyki & Logi", font=("Arial", 12, "bold"), bg="#D8D7DE").grid(row=0, column=0, padx=40, sticky="s")
 
         self.stats_container = tk.Frame(self.bottom_panel, bg="#D8D7DE") #container inside bottom panel for text and statistics
         self.stats_container.grid(row=1, column=0, sticky="n")
 
-        self.stats_best_label = tk.Label(self.stats_container, text="Best Distance: 0.0\nBest cost: 0.0", bg="#D8D7DE",
+        self.stats_best_label = tk.Label(self.stats_container, text="Najlepszy Dystans: 0.0\nNajlepszy koszt: 0.0", bg="#D8D7DE",
                                          font=("Arial", 11), justify="right")
         self.stats_best_label.pack(side=tk.LEFT, padx=50)
 
-        self.stats_present_label = tk.Label(self.stats_container, text="Iteration: 0 \nDistance: 0.0\nCost: 0.0", bg="#D8D7DE", font=("Arial", 11), justify="right")
+        self.stats_present_label = tk.Label(self.stats_container, text="Iteracja: 0 \nDystans: 0.0\nKoszt: 0.0", bg="#D8D7DE", font=("Arial", 11), justify="right")
         self.stats_present_label.pack(side=tk.LEFT, padx=50)
 
-        # Right panel
-        tk.Label(self.right_panel, text="Graphs Visualization", font=("Arial", 12, "bold"), bg="white").pack(pady=5)
+        #-------------------------------Wykresy--------------------------------------------------------------
+        tk.Label(self.right_panel, text="Wizualizacja wykresów", font=("Arial", 12, "bold"), bg="white").pack(pady=5)
 
         self.canvas_container = tk.Frame(self.right_panel, bg="white")
         self.canvas_container.pack(expand=True, fill=tk.BOTH)
-
         # Left graph (the best result)
-        self.canvas_left = tk.Canvas(self.canvas_container, bg="#FAFAFA", highlightthickness=1,
-                                     highlightbackground="#CCCCCC")
+        self.canvas_left = tk.Frame(self.canvas_container, bg="white")
         self.canvas_left.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=5, pady=5)
-
         # Right graph (search animation)
-        self.canvas_right = tk.Canvas(self.canvas_container, bg="#FAFAFA", highlightthickness=1,
-                                      highlightbackground="#CCCCCC")
+        self.canvas_right = tk.Frame(self.canvas_container, bg="white")
         self.canvas_right.pack(side=tk.LEFT, expand=True, fill=tk.BOTH, padx=5, pady=5)
-        # -------------------------------------------------------------------------------
+
+        self.graph_manager = Graphs(self.canvas_left, self.canvas_right)  # klasa do wykresów
+
+
+        #-------------------------------------------------------------------------------
 
 
 
---
+++ core/graphs.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import numpy as np
+
+class Graphs:
+    def __init__(self, frame_left, frame_right):
+        self.fig_left = Figure(figsize=(5, 4), dpi=100)
+        self.fig_right = Figure(figsize=(5, 4), dpi=100)
+
+        self.ax_left = self.fig_left.add_subplot(111)
+        self.ax_right = self.fig_right.add_subplot(111)
+        self.ax_left.set_xlim(0, 100)
+        self.ax_left.set_ylim(0, 100)
+        self.ax_right.set_xlim(0, 100)
+        self.ax_right.set_ylim(0, 100)
+
+        self.canvas_left = FigureCanvasTkAgg(self.fig_left, master = frame_left)
+        self.canvas_left.get_tk_widget().pack(fill = "both", expand=True)
+        self.canvas_right = FigureCanvasTkAgg(self.fig_right, master=frame_right)
+        self.canvas_right.get_tk_widget().pack(fill="both", expand=True)
+
+    def cities_random(self, ile): # metoda do tworzenia randomnych miast na mapie
+        self.ax_left.clear() # wyczyszczamy wykresy
+        self.ax_right.clear()
+
+        x = np.random.randint(0, 101, size = ile) #generujemy miasta
+        y = np.random.randint(0, 101, size = ile)
+
+        self.ax_left.scatter(x, y, color="Blue", s=30)
+        self.ax_right.scatter(x, y, color="Blue", s=30)
+
+        self.ax_left.set_xlim(0, 100)
+        self.ax_left.set_ylim(0, 100)
+        self.ax_right.set_xlim(0, 100)
+        self.ax_right.set_ylim(0, 100)
+
+        self.canvas_left.draw() # rysujemy miasta
+        self.canvas_right.draw()
+
+        return list(zip(x, y)) # zwracamy listę miast dla algorytmów
+
+    def cities_doc(self):
+        pass
