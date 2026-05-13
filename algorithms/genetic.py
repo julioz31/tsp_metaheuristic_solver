e4459624021b3e3ff454b5465c78600d7c1e0bfb
Yuliia Kuzmenko <4projectsanduni@gmail.com>
2026-05-13 11:39:49 +0200

    total distans, koszt, fitness, populacja

--- algorithms/genetic.py
+++ algorithms/genetic.py
@@ -1,5 +1,7 @@
 from math import dist
 from itertools import combinations
+import random as rd
+
 
 class Genetyczny_alg:
     def __init__(self):
@@ -8,8 +10,10 @@ class Genetyczny_alg:
         self.population_size = 100
         self.generations = 100
         self.elitism = True
+        #self.calculate_total_distance = []
 
     def dystans_miasta(self, cities):
+        """kalkulacja dystansu pomiędzy miastami i wnisienie do słownika"""
         self.list_of_cities = cities
         self.distances = {}
 
@@ -21,10 +25,39 @@ class Genetyczny_alg:
 
                     self.distances[(i, j)] = dist(p1, p2)
 
+    def total_dystans(self, route):
+        """kalkulacja całej śzieżki"""
+        total = 0
+        for i in range(len(route) - 1):
+            total += self.distances[(route[i], route[i+1])]
+
+        total += self.distances[(route[-1], route[0])]
+        return total
+
+    def cost(self, route):
+        """kalkulacja efektywności trasy według kosztu
+        koszt = cała trasa/cały priorytet"""
+        total_dist = self.total_dystans(route)
+        total_priority = sum(self.list_of_cities[i][2] for i in route)
+
+        return total_dist / total_priority if total_priority != 0 else total_dist
+
+    def fitness(self, route):
+        """ocena trasy: im mniejszy coszt tym większy fitness"""
+        ocena = self.cost(route)
+
+        return 1 / (ocena + 1e-10)
 
 
     def populacja(self):
-        pass
+        """lista potencyjnych routes"""
+        population = []
+
+        for i in range(self.population_size):
+            route = rd.sample(range(len(self.list_of_cities)), len(self.list_of_cities))
+            population.append(route)
+
+        return population
 
     def crossover(self):
         pass
@@ -35,11 +68,10 @@ class Genetyczny_alg:
     def mutacja(self):
         pass
 
-    def fitness(self):
+    def generation(self):
         pass
 
 
 
 
-    def cost(self):
-        pass
+
