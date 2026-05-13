e265e2d728c8499163d42ecdb3e45f668b8208f5
Yuliia Kuzmenko <4projectsanduni@gmail.com>
2026-05-13 11:07:42 +0200

    dystans pomiędzy miastami

--- algorithms/genetic.py
+++ algorithms/genetic.py
@@ -1,3 +1,5 @@
+from math import dist
+from itertools import combinations
 
 class Genetyczny_alg:
     def __init__(self):
@@ -6,6 +8,19 @@ class Genetyczny_alg:
         self.population_size = 100
         self.generations = 100
         self.elitism = True
+
+    def dystans_miasta(self, cities):
+        self.list_of_cities = cities
+        self.distances = {}
+
+        for i in range(len(cities)):
+            for j in range(len(cities)):
+                if i != j:
+                    p1 = (cities[i][0], cities[i][1])
+                    p2 = (cities[j][0], cities[j][1])
+
+                    self.distances[(i, j)] = dist(p1, p2)
+
 
 
     def populacja(self):
@@ -23,8 +38,8 @@ class Genetyczny_alg:
     def fitness(self):
         pass
 
-    def dystans_miasta(self):
-        pass
+
+
 
     def cost(self):
         pass
